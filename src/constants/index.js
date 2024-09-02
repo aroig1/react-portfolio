a1a0a88994773f169315744fd0e884ee54839b41
aroig1 <aroig@arizona.edu>
2024-09-02 13:44:23 -0700

    react, tailwind css project setup

---
+++ src/constants/index.js
@@ -0,0 +1,41 @@
+export const HERO_CONTENT = `Some hero text`;
+
+export const ABOUT_TEXT = `some about text`;
+
+export const EXPERIENCES = [
+    {
+        year: "insert year",
+        role: "insert role",
+        company: "insert company",
+        description: "insert description",
+        technologies: ["tech 1", "tech 2"]
+    },
+    {
+        year: "insert year",
+        role: "insert role",
+        company: "insert company",
+        description: "insert description",
+        technologies: ["tech 1", "tech 2"]
+    },
+];
+
+export const PROJECTS = [
+    {
+        title: "insert title",
+        description: `insert description`,
+        image: "insert image through import",
+        technologies: ["tech 1", "tech 2"]
+    },
+    {
+        title: "insert title",
+        description: `insert description`,
+        image: "insert image through import",
+        technologies: ["tech 1", "tech 2"]
+    },
+    {
+        title: "insert title",
+        description: `insert description`,
+        image: "insert image through import",
+        technologies: ["tech 1", "tech 2"]
+    },
+];
