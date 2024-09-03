7eef9ca9ff2f58e8163d7eb6a5104f6840f7c201
aroig <aroig@arizona.edu>
2024-09-03 15:35:54 -0700

    Continued experience section

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -2,16 +2,16 @@ import { EXPERIENCES } from "../constants"
 
 export function ShortExperience() {
     return (
-        <div className="border-b border-neutral-900 lg:mx-32">
-            <h2 className="text-3xl text-center">Experience</h2>
+        <div className="border-b border-neutral-900 my-8 lg:mx-32">
+            <h2 className="text-3xl text-center mb-4">Experience</h2>
             <div className="flex justify-evenly">
-                <div>
+                <div className="lg:w-1/4">
                     <h3>{EXPERIENCES[0].company}</h3>
-                    <span>{EXPERIENCES[0].date}</span>
+                    <span className="text-gray-400">{EXPERIENCES[0].date}</span>
                 </div>
-                <div>
-                    <h3>{EXPERIENCES[0].role}</h3>
-                    <p>{EXPERIENCES[0].description}</p>
+                <div className="lg:w-1/2">
+                    <h3 className="mb-2">{EXPERIENCES[0].role}</h3>
+                    <p className="text-gray-400">{EXPERIENCES[0].description}</p>
                 </div>
             </div>
         </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ import { MdOutlineEmail } from "react-icons/md";
 export default function Navbar() {
     return (
         <div className="text-white">
-            <div class="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
+            <div class="fixed top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
             <nav className="flex justify-between items-center p-4">
                 <h1 className="container text-2xl">
                     <Link to="/" className="w-max">Aidan Roig</Link>
