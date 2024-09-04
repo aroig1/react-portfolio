e1f441eab3e5e4f4cf5e3348ce81a8034741535e
aroig1 <aroig@arizona.edu>
2024-09-03 20:50:43 -0700

    margin change to hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@ import { HERO_CONTENT } from "../constants"
 
 export default function Hero() {
     return (
-        <div className="border-b border-neutral-900 pb-4 lg:mb-35 lg:mx-32 flex flex-wrap items-center justify-center lg:justify-evenly">
+        <div className="border-b border-neutral-900 pb-4 mx-6 lg:mb-35 lg:mx-32 flex flex-wrap items-center justify-center lg:justify-evenly">
             <div className="flex flex-col items-center lg:items-start w-full lg:w-1/2">
                 <h1 className="py-10 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl">Aidan Roig</h1>
                 <h2 className="pb-4 bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 
